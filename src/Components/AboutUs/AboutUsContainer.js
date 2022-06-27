17010b481d27a564ddedf8bedd8f40a25f99d79a
PlinytheYounger <whitney.smith.lee@gmail.com>
2022-06-27 13:47:48 +0100

    updated berry picking guidelines page

--- src/Components/AboutUs/AboutUsContainer.js
+++ src/Components/AboutUs/AboutUsContainer.js
@@ -17,6 +17,9 @@ const AboutUsContainer = () => {
             <div className='our-story'>
                 <div>
                     <h3><b>Ferney CommonGround</b> is a small family-owned berry farm and community destination located atop the Blue Ridge Plateau in Floyd, Virginia. We provide visitors a unique agritourism experience with imaginative natural playgrounds and gardens, seasonal pick-your-own berries, and a welcoming, casual space for people of all ages to gather and unwind. Come for the berries, run wild in the gardens, and stay into the golden hour among the flowers.</h3>
+
+                    <h3><b>Address: </b>1825 Ferney Creek Road NW, Willis, VA 24380</h3>
+                    <h3><b>Please Note: </b>All public operations are currently limited to Wednesday - Sunday Only</h3>
                     
                 </div>
                 <div>
--- src/Components/BerryPicking/BerryPickingContainer.js
+++ src/Components/BerryPicking/BerryPickingContainer.js
@@ -15,12 +15,32 @@ const BerryPickingContainer = () => {
                 <div className="berry-container">
                     <div className="first-container">
                         <div className="prices-info">
-                            <h4 className="h4-berry">PRICES</h4>
-                                <p className="p-berry">More info to come!</p>
+                            <h4 className="h4-berry">PRICES (Cash or Zelle Only)</h4>
+                                <p className="p-berry"><b>Blueberries:</b><i> Pick Your Own:</i> $4.50/pound | <i>Ready Picked:</i> $4/pint </p>
                             <h4 className="h4-berry">PICKING SCHEDULE</h4>
-                                <p className="p-berry">More info to come!</p>
+                                <p className="p-berry"><b>Morning Pick:</b> 9 - 11am | <b>Evening Pick:</b> 5 - 8pm.</p>
+                                <p><i>*Midday berry picking is closed due to heat which makes berries and people turn to mush</i></p>
                             <h4 className="h4-berry">BERRY PICKING GUIDELINES</h4>
-                                <p className="p-berry">More info to come!</p>
+                                <ol>
+                                    <li>
+                                        <p className="p-berry">Check-in at the market before berrypicking for a container and directions.</p>
+                                    </li>
+                                    <li>
+                                        <p className="p-berry">No dogs - no exceptions.</p>
+                                    </li>
+                                    <li>
+                                        <p className="p-berry">Leave the green berries. You won’t want to eat them and they will not ripen off the bush.</p>
+                                    </li> 
+                                </ol>
+                            <h4 className="h4-berry">RECOMMENDATIONS</h4>
+                                <ol>
+                                    <li>
+                                        <p className="p-berry">Bring a cooler to store berries on you way home.</p>
+                                    </li>
+                                    <li>
+                                        <p className="p-berry">Do not wash berries until you want to eat them. We do not apply sprays of any kind - often the frozen berries I use in smoothies go right in with all their natural glory. ;)</p>
+                                    </li>
+                                </ol>
                         </div>
                         <div className="pink-rose">
                             <img className="berries" src={berryPhoto1} alt="Beautiful pink rose"></img>
